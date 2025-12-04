84c42748d5abcf16f1c843b9597c32fc6dc64e88
Swetalin Rout <swetalinrout2006@gmail.com>
2025-12-04 20:36:56 +0530

    Deleted all previous files

--- README.md
+++
@@ -1,1 +0,0 @@
-# Weather-Aware-Journal
--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { ensureIDs, renderEntries } from './entries.js';
-import { initEntryForm } from './form.js';
-
-// Initialize the app when the DOM is ready
-window.addEventListener('DOMContentLoaded', () => {
-    ensureIDs();      // Fix any missing IDs
-    renderEntries();  // Show entries on the dashboard
-    initEntryForm();  // Setup form listener if on the new entry page
-});
